7125ff0a47d036d0254389937984be02f2f0985f
dwinurfatimah20 <dwinurfatimaha@gmail.com>
2024-05-01 17:52:28 +0700

    tugas opp desti

--- Ape.php
+++ Ape.php
@@ -1,5 +1,12 @@
 <?php
 require_once "Animal.php";
+class ape extends animal {
+    public function yell() {
+        return "Auooo";
+    }
 
-
+    public function setLegs($legs) {
+        $this->legs = $legs;
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -2,5 +2,27 @@
 
 require_once "Frog.php";
 require_once "Ape.php";
+$sheep = new animal();
+$sheep -> setName("shaun");
 
+echo "Name: ".$sheep -> getName()."<br>";
+echo "Legs : ".$sheep -> legs."<br>";
+echo "Cold blooded : ".$sheep -> cold_blooded()."<br><br>";
+
+$buduk = new frog();
+$buduk -> setName("buduk");
+
+echo "Name: ".$buduk -> getName()."<br>";
+echo "Legs : ".$buduk -> legs."<br>";
+echo "Cold blooded : ".$buduk -> cold_blooded()."<br>";
+echo "Jump : ".$buduk -> jump()."<br><br>";
+
+$sungokong = new ape();
+$sungokong -> setName("kera sakti");
+$sungokong -> setLegs(2);
+
+echo "Name: ".$sungokong -> getName()."<br>";
+echo "Legs: ".$sungokong -> legs."<br>";
+echo "Cold blooded: ".$sungokong -> cold_blooded()."<br>";
+echo "Yell : ".$sungokong -> yell();
 ?>
